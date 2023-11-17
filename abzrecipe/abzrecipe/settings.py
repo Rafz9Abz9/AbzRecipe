e763b8360ca63ac91a4da2da3b8b5bad704612bd
Rafz9Abz9 <rafatramoni15@gmail.com>
2023-11-17 14:12:06 +0000

    Setting up deployment for Heroku

--- abzrecipe/abzrecipe/settings.py
+++ abzrecipe/abzrecipe/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'abzrecipe.urls'
@@ -119,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'), 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # setup media folder
 MEDIA_URL = '/media/'

7c0c351757be82b77179c06ee5e53823db32835c
Rafz9Abz9 <rafatramoni15@gmail.com>
2024-03-08 15:19:27 +0000

    fixed static storage

--- abzrecipe/settings.py
+++ abzrecipe/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")

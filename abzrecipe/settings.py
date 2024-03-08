29d8a8f1db63c1a8891497a66ce77a7c06c018d8
Rafz9Abz9 <rafatramoni15@gmail.com>
2024-03-08 16:01:17 +0000

    fixed static storage

--- abzrecipe/settings.py
+++ abzrecipe/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('APP_SECRET_KEY') 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['abzrecipe-72c82cf99b18.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
